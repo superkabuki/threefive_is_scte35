a5968cf0cb6915343e9df3d5459556f2c121ecee
Adrian of Doom <a@iodisco.com>
2026-02-09 01:36:12 -0500

    Update decodenext.py

--- examples/decodenext.py
+++ examples/decodenext.py
@@ -2,8 +2,16 @@
 next.py demonstrates how to use Stream.decode_next() to grab and process
 the next SCTE-35 Cue in an MPEGTS stream.
 
-Here, if the Cue Splice Command is a Splice Insert, we print the Splice Command vars,
-but you trigger any action you like, this is just an example. 
+threefive.Stream.decode_next() is a python generator that yields a cue
+when it finds one
+
+once you have a cue, you can show it, or pass it to another function or method
+
+Below,
+    when a cue is found,
+    if the splice command has the pts_time,
+    print the splice command name and pts_time
+   else print just the splice command name
 
 """
 
@@ -15,7 +23,10 @@ def do():
     arg = sys.argv[1]
     strm =Stream(sys.argv[1])
     for cue in strm.decode_next():
-        cue.show()
+        cmd = f'Command: {cue.command.name}'
+        if cue.command.has('pts_time'):
+            cmd +=f'\t pts: {cue.command.pts_time}'
+        print(cmd)
 
 
 if __name__ == "__main__":
