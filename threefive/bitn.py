d62aabbc95d9b7aaddd60c26780169760b87ffec
Adrian of Doom <a@iodisco.com>
2026-02-12 06:42:29 -0500

    as_int() & black

--- threefive/bitn.py
+++ threefive/bitn.py
@@ -7,37 +7,37 @@ from .stuff import red, pif
 
 class Bitn:
     """
-    bitn.Bitbin takes a byte string and
-    converts it to a integer, a very large integer.
-    This is used to slice off bits that are not byte-aligned
-    and convert them as needed.
+        bitn.Bitbin takes a byte string and
+        converts it to a integer, a very large integer.
+        This is used to slice off bits that are not byte-aligned
+        and convert them as needed.
 
 
-example:
-                >>> from threefive.bitn import Bitn
-                >>> somebites=b'Byte String'
-                >>> bn=Bitn(somebites)
-                >>> allthebits=bin(bn.bits) # this is Bitn.bits displayed as bits
-                >>> elevenbits=bin(bn.as_int(11)) # this is the first eleven bits 
-                >>> allthebits.startswith(elevenbits)
-                True
-                >>> allthebits
-                '0b100001001111001011101000110010100100000010100110111010001110010011010010110111001100111'
-                >>> elevenbits
-                '0b1000010011'
-                >>> bn.as_hex(5)
-                '0x19'
-                >>> bn.as_int(32)
-                1952784467
-                >>> next4bytes=bn.as_bytes(32)
-                >>> next4bytes
-                b'trin'
+    example:
+                    >>> from threefive.bitn import Bitn
+                    >>> somebites=b'Byte String'
+                    >>> bn=Bitn(somebites)
+                    >>> allthebits=bin(bn.bits) # this is Bitn.bits displayed as bits
+                    >>> elevenbits=bin(bn.as_int(11)) # this is the first eleven bits
+                    >>> allthebits.startswith(elevenbits)
+                    True
+                    >>> allthebits
+                    '0b100001001111001011101000110010100100000010100110111010001110010011010010110111001100111'
+                    >>> elevenbits
+                    '0b1000010011'
+                    >>> bn.as_hex(5)
+                    '0x19'
+                    >>> bn.as_int(32)
+                    1952784467
+                    >>> next4bytes=bn.as_bytes(32)
+                    >>> next4bytes
+                    b'trin'
 
 
     """
 
     def __init__(self, bites):
-        self.bitsize = self.idx = len(bites) << 3
+        self.idx = len(bites) << 3
         self.bits = int.from_bytes(bites, byteorder="big")
 
     def __repr__(self):
@@ -56,10 +56,9 @@ example:
         Starting at self.idx of self.bits,
         slice off num_bits of bits.
         """
-        if self.idx >= num_bits:
-            self.idx -= num_bits
-            return (self.bits >> (self.idx)) & ~(~0 << num_bits)
-        return False
+        #  if self.idx >= num_bits:
+        self.idx -= num_bits
+        return (self.bits >> (self.idx)) & ~(~0 << num_bits)
 
     def as_hex(self, num_bits):
         """
