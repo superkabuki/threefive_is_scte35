16b67073a9a2609d5009d897fd1d1f0ebf902b4c
Adrian of Doom <a@iodisco.com>
2026-02-16 21:53:48 -0500

    reblue

--- threefive/speedo.py
+++ threefive/speedo.py
@@ -8,7 +8,7 @@ calulate data transfer rates.
 
 import sys
 import time
-from .stuff import print2
+from .stuff import print2, reblue
 
 MILLION = 2 << 19
 
@@ -33,11 +33,11 @@ class Speedo:
         mb = self.total_bytes / MILLION
         rate = mb / elapsed
         out = f"\t{mb:0.2f} MB sent in {elapsed:5.2f} seconds. {rate:3.2f} MB/Sec     "
-        self.print_out(out)
+        reblue(out)
 
     def end(self):
         """
         end advance the cursor past the \r
         """
         out = "\n\n"
-        self.print2(out)
+        print2(out)
