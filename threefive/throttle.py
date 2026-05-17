4205df04985cc7496e599ddb953dfe81c3ac9692
Adrian of Doom <a@iodisco.com>
2026-05-16 21:52:31 -0400

    Update throttle.py

--- threefive/throttle.py
+++ threefive/throttle.py
@@ -26,6 +26,19 @@ class Throttle:
         self.ifr = IFramer(shush=True)
         self.shush = shush
 
+    def ssleep(self,duration):
+        """
+        ssleep- a more accurate sleep.
+        """
+        now = time.perf_counter()
+        end = now + duration
+        buff=0.002
+        if duration > buff:
+            time.sleep(duration - buff)
+        # witness on a spin-lock       
+        while time.perf_counter_ns() < end:
+            pass
+
     def _reset(self):
         """
         _reset reinitialize all four stop/start times
@@ -43,7 +56,7 @@ class Throttle:
         _set_start set first and actualstart
         """
         self.first = pts
-        self.actualstart = time.time()
+        self.actualstart = time.perf_counter()
         if not self.shush:
             print2(f"first: {pts} actualstart: {self.actualstart}")
 
@@ -54,28 +67,28 @@ class Throttle:
         if not self.shush:
             print(f"throttling: {diff}", file=sys.stderr, end="\r")
 
-    def _sleep(self, diffed):
+    def _diffsleep(self, diffed):
         """
-        _sleep sleep for diff
+        _diffsleep - sleep for diff
         """
         if 0 < diffed < 10:
             self._print_throttle(diffed)
-            time.sleep(diffed)
+            self.ssleep(diffed)
             self._reset_end()
         else:
             self._reset()
 
     def _diff(self, pts):
         """
-        _diff calculate the difference betweeen start and stop times,
+        _diff - calculate the difference betweeen start and stop times,
         compare pts to actual and sleep the difference.
         """
         if pts > self.first:
-            self.actualstop = time.time()
+            self.actualstop = time.perf_counter()
             ptime = round(pts - self.first, 6)
             atime = round(self.actualstop - self.actualstart, 6)
             diffed = round((ptime - atime) * 0.99, 6)
-            self._sleep(diffed)
+            self._diffsleep(diffed)
 
     def _set_first(self, pts):
         if not self.first:
@@ -87,14 +100,14 @@ class Throttle:
 
     def throttle(self, packet):
         """
-        throttle throttle packet to maintain realtime stream.
+        throttle - throttle packet to maintain realtime stream.
         """
         pts = self.ifr.parse(packet)
         self._throttle_pts(pts)
 
     def _throttle_pts(self, pts):
         """
-        _throttle_pts throttle by pts instead of an mpegts packet
+        _throttle_pts - throttle by pts instead of an mpegts packet
         to maintain a realtime stream.
         """
         if pts:
