3e7ccccd4c0900dad34fa53468efe68e36011806
Adrian of Doom <a@iodisco.com>
2026-08-01 16:53:45 -0400

    Update parsehlstags.py

--- examples/parsehlstags.py
+++ examples/parsehlstags.py
@@ -23,30 +23,38 @@ these are the tags for the segment index_2_8875296.ts
 
 import json
 import sys
-from threefive.hlstags import TagParser
+from threefive.hlstags import TagParser,HEADER_TAGS
 from threefive.new_reader import reader
 
+headers=[]
 data = {}
 for arg in sys.argv[1:]:
     with reader(arg) as mu:
         results = []
         lines = mu.readlines()
         for line in lines:
-            line = line.decode()
-            if line.startswith("#"):
-                # hold tags until we find a segment
-                results.append(line)
-            else:
-                tp = TagParser(results)
-                # when we find a segment, make it a key in data
-                # and a dict of the tags as the value
-                # trim down to just the file name.
-                # "https://example.com/seg137.ts?fu=you" --> "seg137.ts"
-                line = line.rsplit("/", 1)[-1].split("?", 1)[0].strip()
-                if line:
-                    # segment name is the key and the tags are the value.
-                    data[line] = tp.tags
-                    # clear results for next segment
-                    results = []
+                line=line.decode()
+                if line.startswith("#"):
+                    for tag in HEADER_TAGS:
+                        if tag in line:
+                            headers.append(line)
+                    if line not in headers:
+                        # hold tags until we find a segment
+                        results.append(line)         
+                else:
+                    if line:
+                        if headers:
+                            data['HEADER TAGS']=TagParser(headers).tags
+                            headers=[]          
+                        tp = TagParser(results)
+                        # when we find a segment, make it a key in data
+                        # and a dict of the tags as the value
+                        # trim down to just the file name.
+                        line = line.rsplit("/", 1)[-1].split("?", 1)[0].strip()
+                        if line:
+                             # segment name is the key and the tags are the value.
+                            data[line] = tp.tags
+                            # clear results for next segment
+                            results = []
     if data:
         print(json.dumps(data, indent=4))
